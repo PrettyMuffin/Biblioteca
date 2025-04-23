8c532c3d1b0265377dcd395978065d6c06560b80
markobrunello2010@gmail.com <markobrunello2010@gmail.com>
2025-04-23 18:20:45 +0200

    tolto desc appcontext

--- Logica/header/AppContext.h
+++ Logica/header/AppContext.h
@@ -1,17 +1,3 @@
-/*
-Design pattern Service Locator
-Vantaggi del Service Locator
-1. Accesso centralizzato: Puoi accedere alla Biblioteca da qualsiasi parte del
-programma senza dover passare l'istanza come parametro.
-
-2. Flessibilità: Puoi cambiare l'implementazione della Biblioteca (ad esempio,
-usando un mock per i test) senza modificare il codice che la utilizza.
-
-2. Chiarezza: Il codice che utilizza la Biblioteca non deve sapere come essa
-viene creata o gestita.
-
-*/
-
 #include "Biblioteca.h"
 #include "qcoreapplication.h"
 #include <stdexcept>
